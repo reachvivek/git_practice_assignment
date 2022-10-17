be6af8b6c81aaa08dfad3635e6cb41993b753651
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-17 19:46:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ headerTitle.style.borderBottom="solid 3px #000"
 var addItem=document.querySelector('.title')
 addItem.style.fontWeight="900"
 addItem.style.color="green"
+
+//Get Elements by Class Name
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+items[2].style.backgroundColor="green"
+for (let i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold'
+}
