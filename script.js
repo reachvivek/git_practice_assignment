acaad2c8038b3b90155e5efa7c63be8481d04b38
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-17 23:42:41 +0530

    Updated Objects in Local Storage

--- script.js
+++ script.js
@@ -9,7 +9,43 @@ function submit(){
     }
     else{
         alert("Submitted Successfully!")
-        localStorage.setItem('name',nameInp.value)
-        localStorage.setItem('email',email.value)
+        if (localStorage.getItem('userDetails')==null){
+            let details={
+                name:nameInp.value,
+                email:email.value
+            }
+            localStorage.setItem('userDetails',[JSON.stringify(details)])
+            console.log(localStorage.getItem('userDetails'))
+        }
+        else{
+            let prev=localStorage.getItem('userDetails')
+            let array=[]
+            array.push(prev)            
+            let details={
+                name:nameInp.value,
+                email:email.value
+            }
+            array.push(JSON.stringify(details))
+            localStorage.removeItem('userDetails')
+            localStorage.setItem('userDetails', array)
+            console.log(localStorage.getItem('userDetails'))
+        }
+        // if (localStorage.getItem(userDetails)==null){
+        //     let userDetails={
+        //         name:nameInp.value,
+        //         email:email.value
+        //     }
+        //     localStorage.setItem('userDetails',JSON.stringify(userDetails))
+        // }
+        // else {
+        //     let arr=localStorage.getItem('userDetails')
+        //     let userDetails={
+        //         name:nameInp.value,
+        //         email:email.value
+        //     }
+        //     arr.push(JSON.stringify(userDetails))
+        //     localStorage.setItem('userDetails', arr)
+        // }
+        
     }
 }
