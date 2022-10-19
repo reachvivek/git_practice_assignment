a6b00b38113a435ea951c07c3be489d1b034c2f2
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-20 02:31:48 +0530

    Posts and timer callback

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-// let multiply = (x,y){
-//     console.log(x*y)
-// }
+const posts=[
+	{title: "Post 1", body:"This is Post 1", createdAt: new Date(new Date().setTime(new Date().getTime()-5990000))},
+	{title: "Post 2", body:"This is Post 2", createdAt: new Date(new Date().setTime(new Date().getTime()-200000))}
+]
 
-let multiply=function(x){
-    return function(y){
-        console.log(x*y)
-    }
+function getPosts(){
+	setTimeout(()=>{
+		let output=''
+		posts.forEach(post=>{
+			output+=`<li>${post.title} : ${post.body} - created ${timeSince(post.createdAt)}</li>`
+		})
+		document.querySelector('.msg').innerHTML=output
+	}, 1000)
 }
 
-let multiplyByTwo=multiply(2)
-multiplyByTwo(2)
+function timeSince(date) {
+	var seconds = Math.floor((new Date() - date) / 1000);
+	var interval = seconds / 31536000;
+	if (interval > 1) {
+		return Math.floor(interval) + " years ago";
+	}
+	interval = seconds / 2592000;
+	if (interval > 1) {
+		return Math.floor(interval) + " months ago";
+	}
+	interval = seconds / 86400;
+	if (interval > 1) {
+		return Math.floor(interval) + " days ago";
+	}
+	interval = seconds / 3600;
+	if (interval > 1) {
+		return Math.floor(interval) + " hours ago";
+	}
+	interval = seconds / 60;
+	if (interval > 1) {
+		return Math.floor(interval) + " minutes ago";
+	}
+	return Math.floor(seconds) + " seconds ago";
+}
+  
 
-// let multiplyByThree=multiply.bind(this, 3)
-// multiplyByThree(5)
+setInterval(getPosts, 1000) 
 
+function createPost(post, callback, time) {
+	setTimeout(()=>{
+		post.createdAt=new Date()
+		posts.push(post)
+		callback()
+	}, time);
+}
+
+
+let i=3;
+
+while (i<10){
+	createPost({title: "Post "+i, body:"This is Post "+i, createdAt: ""}, getPosts, i*3000)
+	i++
+}
