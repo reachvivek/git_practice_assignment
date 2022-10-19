e239792e54e10e5d022e50238f1e5a7c80bb38de
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-19 08:46:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,16 @@
-let nameInp = document.querySelector('#name')
-let email = document.querySelector('#email')
-let btn=document.querySelector('.btn')
-btn.addEventListener('click', submit)
+// let multiply = (x,y){
+//     console.log(x*y)
+// }
 
-let arr = JSON.parse(localStorage.getItem('userDetails'))
-if (arr){
-    arr.map((item, ind)=>{
-        var li=document.createElement('li')
-        var editBtn=document.createElement('button')
-        editBtn.className='editBtn'
-        editBtn.appendChild(document.createTextNode('Edit'))
-        editBtn.style.float='right'
-        editBtn.id=ind
-        var delBtn=document.createElement('button')
-        delBtn.className='delBtn'
-        delBtn.id=ind
-        delBtn.appendChild(document.createTextNode('Delete'))
-        delBtn.style.float='right'
-        li.className='list-group-item'
-        var det=document.createTextNode(`${item.name} ${item.email}`)
-        li.append(det)
-        li.append(delBtn)
-        li.append(editBtn)
-        items=document.getElementById('users')    
-        items.appendChild(li)
-    })
-}
-let editBtn=document.querySelectorAll('.editBtn')
-let delBtn=document.querySelectorAll('.delBtn')
-for(let i=0; i<delBtn.length; i++){
-    delBtn[i].addEventListener('click', deleteItem)
-    editBtn[i].addEventListener('click', editItem)
-}
-
-function editItem(e){
-    console.log(e.target.id)
-    nameInp.value=arr[e.target.id].name
-    email.value=arr[e.target.id].email
-    arr.splice(e.target.value, 1)
-    localStorage.removeItem('userDetails')
-    localStorage.setItem('userDetails', JSON.stringify(arr))
-}
-
-function deleteItem(e){
-    console.log(e.target.id)
-    arr.splice(e.target.value, 1)
-    localStorage.removeItem('userDetails')
-    localStorage.setItem('userDetails', JSON.stringify(arr))
-    location.reload()
-}
-
-function submit(){
-    if (nameInp.value.length<3 || email.value.indexOf('@')==-1){
-        alert("Enter valid input!")
-    }
-    else{
-        alert("Submitted Successfully!")
-        if (localStorage.getItem('userDetails')==null){
-            let details=[{
-                name:nameInp.value,
-                email:email.value
-            }]
-            localStorage.setItem('userDetails',JSON.stringify(details))
-            console.log(localStorage.getItem('userDetails'))
-        }
-        else{
-            let prev=JSON.parse(localStorage.getItem('userDetails'))
-            let duplicate;
-            prev.map((it, ind) => {
-                if (it.email==email.value){
-                    duplicate=ind
-                }
-            });         
-            
-            if (duplicate!==undefined){
-                prev.splice(duplicate, 1)
-                console.log(prev)
-            }
-
-            let details={
-                name:nameInp.value,
-                email:email.value
-            }
-            prev.push(details)
-            localStorage.removeItem('userDetails')
-            localStorage.setItem('userDetails', JSON.stringify(prev))
-            console.log(localStorage.getItem('userDetails'))
-            
-        }
-        location.reload()
+let multiply=function(x){
+    return function(y){
+        console.log(x*y)
     }
 }
+
+let multiplyByTwo=multiply(2)
+multiplyByTwo(2)
+
+// let multiplyByThree=multiply.bind(this, 3)
+// multiplyByThree(5)
+
